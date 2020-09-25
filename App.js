0eb6a112afdd887d6616f0d963e717b5c14ce47d
Karem1986 <karem.ortiz@outlook.com>
2020-09-25 12:13:33 +0200

    make a mini mobile view first version

--- App.js
+++ App.js
@@ -1,12 +1,34 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { Button, StyleSheet, Text, TextInput, View } from "react-native";
 
 export default function App() {
+  const [value, onChangeText] = React.useState(" Happy Friday!");
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <TextInput
+        style={{
+          height: 70,
+          width: 130,
+          color: "turquoise",
+          backgroundColor: "purple",
+          padding: 20,
+          margin: 30,
+        }}
+        onChangeText={(i) => onChangeText(i)}
+        value={value}
+      ></TextInput>
+
+      <Button
+        title="Start playing"
+        onPress={() => console.log("a click")}
+        color="#564890"
+        accessibilityLabel="Learn more about cats by adopting a virtual one!"
+      />
       <StatusBar style="auto" />
+      <View>
+        <Text color="#fff">We hope you enjoy!</Text>
+      </View>
     </View>
   );
 }
@@ -14,8 +36,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
