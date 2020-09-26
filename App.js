9a152c6918d7e8879489b827f070bf3c43d535be
Karem1986 <karem.ortiz@outlook.com>
2020-09-26 11:45:21 +0200

    Get the value the user enters with useState hook

--- App.js
+++ App.js
@@ -1,34 +1,32 @@
 import { StatusBar } from "expo-status-bar";
-import React from "react";
-import { Button, StyleSheet, Text, TextInput, View } from "react-native";
+import React, { useState } from "react";
+import { Button, StyleSheet, Image, Text, TextInput, View } from "react-native";
 
 export default function App() {
-  const [value, onChangeText] = React.useState(" Happy Friday!");
+  //1.First we grab the value the user enters with state
+  const [addItem, setAddItem] = React.useState("");
+  //2. Second, we store the values the user enters, that is an [] initialState
+
   return (
     <View style={styles.container}>
+      <View>
+        <Text style={styles.text}>Add your Kermit List and get on track!</Text>
+      </View>
+      <Image style={styles.cat} source={require("./assets/cat.jpeg")} />
       <TextInput
-        style={{
-          height: 70,
-          width: 130,
-          color: "turquoise",
-          backgroundColor: "purple",
-          padding: 20,
-          margin: 30,
-        }}
-        onChangeText={(i) => onChangeText(i)}
-        value={value}
+        style={styles.textInput}
+        placeholder="Add your item"
+        onChangeText={(text) => setAddItem(text)}
+        value={addItem}
       ></TextInput>
 
       <Button
-        title="Start playing"
+        title="Submit"
         onPress={() => console.log("a click")}
         color="#564890"
         accessibilityLabel="Learn more about cats by adopting a virtual one!"
       />
       <StatusBar style="auto" />
-      <View>
-        <Text color="#fff">We hope you enjoy!</Text>
-      </View>
     </View>
   );
 }
@@ -39,5 +37,22 @@ const styles = StyleSheet.create({
     backgroundColor: "#fff",
     alignItems: "center",
     justifyContent: "center",
+  },
+  cat: {
+    width: 80,
+    height: 90,
+  },
+  text: {
+    padding: 20,
+    fontSize: 15,
+    fontWeight: "bold",
+  },
+  textInput: {
+    padding: 10,
+    marginTop: 10,
+    borderColor: "#345678",
+    marginBottom: 10,
+    borderWidth: 2,
+    borderColor: "gray",
   },
 });
