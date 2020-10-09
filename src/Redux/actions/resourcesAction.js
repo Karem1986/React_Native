3bc2481f166b994d4395d04bb027def8093d5162
Karem1986 <karem.ortiz@outlook.com>
2020-10-09 12:34:12 +0200

    created thunk to fetch api data

--- src/Redux/actions/resourcesAction.js
+++ src/Redux/actions/resourcesAction.js
@@ -1,9 +1,21 @@
 export const FETCH_ARTICLES = 'FETCH_ARTICLES'
+import axios from "axios";
 
-
-export const fetchArticles = ()=> {
+export function displayArticles(articles) {
   return {
-    type: FETCH_ARTICLES,
-    payload: {id: 1, title: "Tech stuff",description: "Best CSS tricks from Kermit"}
-  };
+    type: "FETCH_ARTICLES",
+    payload: articles
+  }
+}
+
+
+export async function fetchArticles(dispatch, getState) {
+  console.log('TEST THUNK')
+ 
+  //     const response = await axios.get('http://newsapi.org/v2/top-headlines?sources=techcrunch&apiKey=0d97d0fa44394ed0a197fb149dd40002')
+  //     const result = response.data
+  //     console.log('testing with axios the result', result)
+  
+  // dispatch(displayArticles(result))
+  
 };
