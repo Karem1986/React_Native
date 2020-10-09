96745982ba3ff57301da7a11d0909eb044de2f90
Karem1986 <karem.ortiz@outlook.com>
2020-10-09 13:25:02 +0200

    created thunk to fetch articles from api

--- src/Redux/actions/resourcesAction.js
+++ src/Redux/actions/resourcesAction.js
@@ -12,10 +12,12 @@ export function displayArticles(articles) {
 export async function fetchArticles(dispatch, getState) {
   console.log('TEST THUNK')
  
-  //     const response = await axios.get('http://newsapi.org/v2/top-headlines?sources=techcrunch&apiKey=0d97d0fa44394ed0a197fb149dd40002')
-  //     const result = response.data
-  //     console.log('testing with axios the result', result)
+      const response = await axios.get('http://newsapi.org/v2/top-headlines?sources=techcrunch&apiKey=0d97d0fa44394ed0a197fb149dd40002')
+      const result = response.data
+      console.log('testing with axios the result', result)
   
-  // dispatch(displayArticles(result))
+  dispatch(displayArticles(result))
   
 };
+
+//Dispatch the action in the component: Extra page 
