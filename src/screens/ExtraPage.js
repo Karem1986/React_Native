a83b329514458c1062037cf60e4ab89f81aa2ce9
Karem1986 <karem.ortiz@outlook.com>
2020-10-12 11:04:13 +0200

    made a nice style for articles

--- src/screens/ExtraPage.js
+++ src/screens/ExtraPage.js
@@ -1,6 +1,6 @@
 //RESOURCES PAGE TEST 
 import React, { useEffect } from "react";
-import { StyleSheet, View, Text, FlatList } from "react-native";
+import { SafeAreaView, StyleSheet, View, Text, FlatList, StatusBar} from "react-native";
 import {useDispatch, useSelector} from "react-redux"
 import {fetchArticles} from '../Redux/actions/resourcesAction'
 
@@ -13,9 +13,12 @@ useEffect(() => {
 
  const articles = useSelector(state => state.news.articles)
 console.log('articles', articles)
-const Item = ({ title, author, content, description }) => (
+const Item = ({ title, content, author}) => (
   <View>
-    <Text >{title, author, content, description}</Text>
+    <Text style={styles.author}> - {author}</Text>
+    <Text style={styles.title}>{title}</Text>
+<Text style={styles.content}>{content}</Text>
+
   </View>
 );
 const App = () => {
@@ -28,25 +31,23 @@ const App = () => {
   );
 }
   return (
+      
    
-    <View style={styles.container}>
-      <Text>Extra page</Text>
-      <View>
-       <FlatList 
-       data={articles.articles} 
-       keyExtractor={item => item.url}
-       renderItem={({item}) => (
-        <Item title={item.title}
-        author={item.author}
-        content={item.content}
-        description={item.description}
-         />
-       )}
-       />
-    </View>
-    </View>
-   
-
+          <SafeAreaView style={styles.articlesStyle}>
+           
+              <FlatList 
+          data={articles.articles} 
+          keyExtractor={item => item.url}
+          renderItem={({item}) => (
+           <Item title={item.title}
+           content={item.content}
+           author={item.author}
+       
+            />
+          )}
+          />
+          </SafeAreaView>
+  
   );
 }
 
@@ -57,4 +58,32 @@ const styles = StyleSheet.create({
     alignItems: "center",
     justifyContent: "center",
   },
+
+  articlesStyle: {
+    padding: 50,
+    marginTop: StatusBar.currentHeight || 0,
+    backgroundColor: "#6a2c70",
+    alignItems: "center",
+    justifyContent: "center",
+    fontSize: 10,
+    paddingBottom: 25
+  },
+  title: {
+    fontSize: 22,
+    color: "#19d3da",
+    paddingBottom: 20
+  },
+  author: {
+    paddingTop: 25,
+    paddingBottom: 15,
+    fontFamily: 'monospace',
+    color: "#ee6f57",
+  
+
+  },
+  content: {
+    color: "#f6f5f5",
+    fontFamily: "sans-serif",
+    fontSize: 15
+  }
 })
